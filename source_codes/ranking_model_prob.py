98b0a256ae83bf602c240f01432c0b2e90b56492
zluor <rivenluo01@gmail.com>
2023-07-23 19:37:40 +0800

    1.Add data_aug.py 2.Add q1_ranking_model_tuning.ipynb 3.Add annotation to the ranking_model_prob.py

--- source_codes/ranking_model_prob.py
+++ source_codes/ranking_model_prob.py
@@ -1,17 +1,23 @@
-import os
-import keras_tuner as kt
-from tensorflow import keras
-import numpy as np
-import csv
-import json
-from keras import Input, optimizers, regularizers, callbacks, models, Model, Sequential, metrics, backend as K, losses
-from keras.applications import vgg19, xception, efficientnet, resnet, inception_resnet_v2, VGG19, Xception, \
-    EfficientNetB7, ResNet152V2, InceptionResNetV2
+from keras import Model, Input, layers, Sequential
+from keras.applications import VGG19
+from keras.layers import Dense, Flatten, BatchNormalization, Dropout, Subtract, Activation
+from keras.optimizers import Adam, SGD, RMSprop
 
-def create_ranking_score(img_size):
+
+def create_ranking_network(dense_units, dropout_rate, activation, img_size):
     """
+    Create  ranking network which gives a score to an image.
 
-    :return:
+    :param img_size:
+    :type img_size: int
+    :param dense_units:
+    :type dense_units: int
+    :param dropout_rate:
+    :type dropout_rate: float
+    :param activation:
+    :type activation: str
+    :return: ranking network
+    :rtype: keras.Model
     """
     # Create feature extractor from VGG19
     feature_extractor = VGG19(weights="imagenet", include_top=False, input_shape=(img_size, img_size, 3))
@@ -19,36 +25,77 @@ def create_ranking_score(img_size):
         layer.trainable = False
 
     # Add dense layers on top of the feature extractor
-    input_img = Input(shape=(img_size, img_size, 3), name='input_image')
-    base = feature_extractor(input_img)
+    inp = Input(shape=(img_size, img_size, 3), name='input_image')
+    base = feature_extractor(inp)
     base = Flatten(name='Flatten')(base)
 
     # Block 1
-    base = Dense(32, activation='relu', name='Dense_1')(base)
+    base = Dense(dense_units, activation=activation, name='Dense_1')(base)
     base = BatchNormalization(name='BN1')(base)
-    base = Dropout(0.490, name='Drop_1')(base)
+    base = Dropout(dropout_rate, name='Drop_1')(base)
 
     # Block 2
-    base = Dense(128, activation='relu', name='Dense_2')(base)
+    base = Dense(dense_units, activation=activation, name='Dense_2')(base)
     base = BatchNormalization(name='BN2')(base)
-    base = Dropout(0.368, name='Drop_2')(base)
+    base = Dropout(dropout_rate, name='Drop_2')(base)
 
     # Final dense
     base = Dense(1, name="Dense_Output")(base)
     base_network = Model(inp, base, name='Scoring_model')
     return base_network
 
-def create_meta_network(img_size, weights=None):
-    """
 
-    :param img_size:
-    :param weights:
-    :return:
+def create_meta_network(
+        dense_units,
+        dropout_rate,
+        learning_rate,
+        optimizer,
+        activation,
+        learning_rate_decay,
+        img_size,
+        data_aug=True,
+        weights=None
+):
+    """
+    Create meta network which is used to teach the ranking network.
+
+    :param img_size: dimension of input images during training.
+    :type img_size: tuple(int)
+    :param dense_units: number of units in the dense layer
+    :type dense_units: int
+    :param dropout_rate: dropout rate
+    :type dropout_rate: float
+    :param learning_rate: learning rate for the optimizer
+    :type learning_rate: float
+    :param optimizer: optimizer for model training
+    :type optimizer: str
+    :param activation: activation function for model layers
+    :type activation: str
+    :param learning_rate_decay: learning rate decay rate
+    :type learning_rate_decay: float
+    :param weights: path to the weights use for initialization
+    :type weights: str
+    :return: meta network model
+    :rtype: keras.Model
     """
     # Create the two input branches
     input_left = Input(shape=(img_size, img_size, 3), name='left_input')
     input_right = Input(shape=(img_size, img_size, 3), name='right_input')
-    base_network = create_ranking_network(img_size)
+
+    if data_aug:
+        # Augment data with linear transformation
+        data_augmentation = Sequential(
+            [
+                layers.RandomTranslation(0.2, 0.2, fill_mode="reflect", interpolation="bilinear", ),
+                layers.RandomFlip("horizontal"),
+                layers.RandomRotation(0.1),
+                layers.RandomZoom(0.2)
+            ]
+        )
+        input_left = data_augmentation(input_left)
+        input_right = data_augmentation(input_right)
+
+    base_network = create_ranking_network(img_size, dense_units, dropout_rate, activation)
     left_score = base_network(input_left)
     right_score = base_network(input_right)
 
@@ -57,12 +104,21 @@ def create_meta_network(img_size, weights=None):
 
     # Pass difference through sigmoid function.
     prob = Activation("sigmoid", name="Activation_sigmoid")(diff)
-    model = Model(inputs=[input_left, input_right], outputs= prob, name="Meta_Model")
+    model = Model(inputs=[input_left, input_right], outputs=prob, name="Meta_Model")
 
     if weights:
         print('Loading weights ...')
         model.load_weights(weights)
 
-    model.compile(optimizer=Adam(learning_rate=1e-6), loss="binary_crossentropy", metrics=['accuracy'])
+    if optimizer == "adam":
+        optimizer = Adam(learning_rate=learning_rate, decay=learning_rate_decay)
+    elif optimizer == "sgd":
+        optimizer = SGD(learning_rate=learning_rate, decay=learning_rate_decay)
+    elif optimizer == "rmsprop":
+        optimizer = RMSprop(learning_rate=learning_rate, decay=learning_rate_decay)
+    else:
+        raise ValueError("Unsupported optimizer: " + optimizer)
+
+    model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=['accuracy'])
 
     return model
